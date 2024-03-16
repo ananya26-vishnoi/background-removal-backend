7f61c6490d743dd06628943ea9d815eded63318d
ananya26-vishnoi <ananyavishnoi26@gmail.com>
2024-03-16 15:46:12 +0530

    eddited

--- imageapi/views.py
+++ imageapi/views.py
@@ -2,18 +2,31 @@ import base64
 import requests
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from PIL import Image
 
 BITLY_ACCESS_TOKEN = '2e5e5bb79324c5410a16b19df7b34dc97a8c6c04'  # Replace with your Bitly access token
 
 @api_view(['POST'])
 def upload_and_remove_background(request):
-        try:
-            uploaded_image = request.FILES['image']
+            uploaded_image = request.data['image']
+            print(uploaded_image)
+            
+            image = Image.open("file_path.jpeg")
 
+                # Process the image as needed
+                # For example, you can resize the image:
+            resized_image = image.resize((400, 500))
+
+                # Save or further process the image
+            resized_image.save('path_to_save_resized_image')
+
+                # Return a success response
+            # return JsonResponse({'message': 'Image processed successfully'}, status=200)
+            print("ded_image")
             # Save the uploaded image locally
-            with open('uploaded_image.jpg', 'wb') as f:
-                for chunk in uploaded_image.chunks():
-                    f.write(chunk)
+            # with open('uploaded_image.jpg', 'wb') as f:
+            #     for chunk in uploaded_image.chunks():
+            #         f.write(chunk)
 
             # Use remove.bg API to remove the background
             api_key = 'Tb94jCjUMck7bngZHmNFcPyn'
@@ -34,6 +47,3 @@ def upload_and_remove_background(request):
                 result = {'success': False, 'error': 'Failed to remove background.'}
 
             return JsonResponse(result)
-
-        except Exception as e:
-            return JsonResponse({'success': False, 'error': str(e)})
