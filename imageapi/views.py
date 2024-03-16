dbae6882576debfcd930b16dfcd76551b7851942
ananya26-vishnoi <ananyavishnoi26@gmail.com>
2024-03-16 21:08:52 +0530

    updated backend for react native

--- imageapi/views.py
+++ imageapi/views.py
@@ -1,40 +1,35 @@
 import base64
 import requests
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
-from PIL import Image
+import string
+import base64
+import random
+from rest_framework.response import Response
+from rest_framework import status
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 BITLY_ACCESS_TOKEN = '2e5e5bb79324c5410a16b19df7b34dc97a8c6c04'  # Replace with your Bitly access token
 
 @api_view(['POST'])
 def upload_and_remove_background(request):
         try:
-            uploaded_image = request.data['image']
-            print(uploaded_image)
-            
-            image = Image.open("file_path.jpeg")
-
-                # Process the image as needed
-                # For example, you can resize the image:
-            resized_image = image.resize((400, 500))
-
-                # Save or further process the image
-            resized_image.save('path_to_save_resized_image')
-
-                # Return a success response
-            # return JsonResponse({'message': 'Image processed successfully'}, status=200)
-            print("ded_image")
-            # Save the uploaded image locally
-            # with open('uploaded_image.jpg', 'wb') as f:
-            #     for chunk in uploaded_image.chunks():
-            #         f.write(chunk)
+            image = request.FILES['image'].read()
+            image = b'\n' + image
+            dd = image.hex()
+            data = bytes.fromhex(dd[2:])  
+            random_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+            path = 'media/' + random_name + '.jpg'
+            with open(path, 'wb') as file:
+                file.write(data)  
 
             # Use remove.bg API to remove the background
             api_key = 'Tb94jCjUMck7bngZHmNFcPyn'
             response = requests.post(
                 'https://api.remove.bg/v1.0/removebg',
                 headers={'X-Api-Key': api_key},
-                files={'image_file': open('uploaded_image.jpg', 'rb')},
+                files={'image_file': open(path, 'rb')},
             )
 
             # Check if the API call was successful
@@ -42,12 +37,20 @@ def upload_and_remove_background(request):
                 # Encode the image content as base64
                 image_content_base64 = base64.b64encode(response.content).decode('utf-8')
 
-                # Provide the base64-encoded image content in the response
-                result = {'success': True, 'image': image_content_base64}
-            else:
-                result = {'success': False, 'error': 'Failed to remove background.'}
+                # save this image to the media folder
+                with open(path, 'wb') as file:
+                    file.write(response.content)
 
-            return JsonResponse(result)
+                # Provide the base64-encoded image content in the response
+                result = {'success': True, 'image': image_content_base64, 'url' : os.getenv('BASE_URL') + "/"+path}
+                return Response(result, status=status.HTTP_200_OK)
+            else:
+                print(response.content)
+                result = {'success': False, 'error': 'Failed to remove background.'}
+                return Response(result, status=status.HTTP_400_BAD_REQUEST)
+            
 
         except Exception as e:
-            return JsonResponse({'success': False, 'error': str(e)})
+            print(e)
+            result = {'success': False, 'error': e}
+            return Response(result, status=status.HTTP_400_BAD_REQUEST)
